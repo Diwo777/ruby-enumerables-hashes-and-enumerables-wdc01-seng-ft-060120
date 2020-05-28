5b98b1e9102061bfa79fad010c5f67fabbcda412
Diego Zegarra <diego.zegarra79@icloud.com>
2020-05-28 15:50:40 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -7,7 +7,10 @@ require 'pry'
 
 def happy_birthday(birthday_kids)
   birthday_kids.each do |kid, age| 
-    
+    counter = 0 
+    while counter < 3 
+    puts "Happy Birthday #{kid[counter]}! You are now #{age[counter]} years old!"
+    counter++
  end 
    
   
