38ec63addaf31cbd76e2fbde953bdc285968dc75
Diego Zegarra <diego.zegarra79@icloud.com>
2020-05-28 15:36:04 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -9,9 +9,9 @@
 def select_winner(passengers)
   passengers.each do |key,value|
     if key == :suite_a && value.start_with?("A")
-      winner = name 
+      winner = value
   end 
-  # add the code snippet here!
+ 
 end
 winner 
 end
